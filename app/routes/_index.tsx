16d48bd40c48fa8ed036ae8f6244fcbb7367182e
Ivan Sheva <sevcenkoivan673@gmail.com>
2024-07-04 18:21:01 +0300

    Init, install pocketbase and shadcn

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,7 @@
-import type { MetaFunction } from "@remix-run/node";
+import { json, type MetaFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { Button } from "~/components/ui/button";
+import { pb } from "~/lib/pb";
 
 export const meta: MetaFunction = () => {
   return [
@@ -7,42 +10,17 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export const loader = async () => {
+  const users = await pb.collection("users").getFullList();
+  return json({ users });
+};
+
 export default function Index() {
+  const data = useLoaderData<typeof loader>();
+  console.log(data.users);
   return (
     <div className="font-sans p-4">
-      <h1 className="text-3xl">Welcome to Remix</h1>
-      <ul className="list-disc mt-4 pl-6 space-y-2">
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/quickstart"
-            rel="noreferrer"
-          >
-            5m Quick Start
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/start/tutorial"
-            rel="noreferrer"
-          >
-            30m Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            className="text-blue-700 underline visited:text-purple-900"
-            target="_blank"
-            href="https://remix.run/docs"
-            rel="noreferrer"
-          >
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      <Button>Click me!</Button>
     </div>
   );
 }

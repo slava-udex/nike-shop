07915716a054bfde1ad90bf8361b59388863f233
Ivan Sheva <sevcenkoivan673@gmail.com>
2024-07-06 10:52:06 +0300

    Log in to pocketbase

--- app/lib/pb.ts
+++ app/lib/pb.ts
@@ -1,3 +1,9 @@
 import PocketBase from "pocketbase";
 
 export const pb = new PocketBase("http://localhost:8090");
+
+// Login as an admin
+await pb.admins.authWithPassword(
+  process.env.POCKETBASE_EMAIL!,
+  process.env.POCKETBASE_PASSWORD!
+);
